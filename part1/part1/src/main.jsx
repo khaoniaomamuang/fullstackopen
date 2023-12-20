efddb74f1e0a395cba91d7c4db634e381ba5e7c5
khaoniaomamuang <26197169+khaoniaomamuang@users.noreply.github.com>
2023-12-21 05:14:53 +0700

    part1update

--- part1/part1/src/main.jsx
+++ part1/part1/src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+
+// The file App.jsx now defines a React component with the name App. The command on the final line of file main.jsx renders its contents into the div-element, defined in the file index.html, having the id value 'root'.
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
