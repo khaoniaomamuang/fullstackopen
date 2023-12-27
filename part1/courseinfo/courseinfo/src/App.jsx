dd5625a77abec4311d55aca78062abc55474f746
khaoniaomamuang <26197169+khaoniaomamuang@users.noreply.github.com>
2023-12-27 16:00:53 +0700

    1.5

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.part.name}</h1>;
 };
 
 const Content = (props) => {
@@ -27,27 +27,30 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={parts} />
-      <Total part={parts} />
+      <Header part={course} />
+      <Content part={course.parts} />
+      <Total part={course.parts} />
     </div>
   );
 };
